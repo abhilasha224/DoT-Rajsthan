b787bfa1b6b69c4b010755739fe464a9f7eaedc5
abhilasha <abhilashalad999@gmail.com>
2017-03-20 13:44:28 +0530

    version 2

--- server/api.js
+++ server/api.js
@@ -1,29 +1,42 @@
 var express = require('express');
-var http = require('http');
+var https = require('https');
 var app = express();
 var fs = require("fs");
 
 app.get('/getUsers', function (req, res) {
-  var options = {
-  host: "apitest.sewadwaar.rajasthan.gov.in",
-  port: 80,
-  path: '/app/live/Service/hofAndMember/ForApp/WDYYYGG?client_id=ad7288a4-7764-436d-a727-783a977f1fe1',
-  method: 'GET'
-};
-console.log(options);
-http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
+  https.get("https://apitest.sewadwaar.rajasthan.gov.in/app/live/Service/hofAndMember/ForApp/WDYYYGG?client_id=ad7288a4-7764-436d-a727-783a977f1fe1",
+    function(response,body){
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      var bhamData = JSON.parse(chunk);
+      console.log(bhamData);
+      console.log('BODY: ',bhamData.hof_Details);
+      var bhamDataJson = [];
+      // get hof details
+      var tempobj = {};
+      tempobj.bhamashah_id = bhamData.hof_Details.BHAMASHAH_ID;
+      tempobj.member_id = bhamData.hof_Details.M_ID;
+      bhamDataJson.push(tempobj);
+
+
+      // get Members details
+      for(var memData in bhamData.family_Details)
+      {
+        console.log("memData",memData);
+        var tempobj = {};
+        tempobj.bhamashah_id = memData.BHAMASHAH_ID;
+        tempobj.member_id = memData.M_ID;
+        bhamDataJson.push(tempobj);
+      }
+        console.log("bhamDataJson",bhamDataJson);
+      res.status(200).send(chunk);
+    });
   });
-}).end();
-  console.log("listing users");
 });
 
 app.get('/api', function(){
   console.log("API API*****");
+
 });
 
 var server = app.listen(8081, function () {
